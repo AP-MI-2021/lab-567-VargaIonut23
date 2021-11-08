35f217b8748f955081ebcf0efc448d1a690bf33e
VargaIonut23 <91500038+VargaIonut23@users.noreply.github.com>
2021-11-08 18:57:33 +0200

    lab 7

--- UserInterface/command_line_console.py
+++ UserInterface/command_line_console.py
@@ -1,10 +1,10 @@
 from Domain.rezervare import toString
-from Logic.CRUD import adaugarezervare, stergerezervare
+from Logic.CRUD import adaugarezervare, stergerezervare, modificarezervare
 
 
 def printDescriere():
     print("Lista de comenzi va fi separat prin ; ")
-    print("Comenzile pot fi: add, showall, delete, exit ")
+    print("Comenzile pot fi: add, showall, delete, update,exit ")
     print("Parametri dintr-o comanda vor fi separati prin ,")
 
 
@@ -29,6 +29,14 @@ def uishowall(lista):
         print(toString(rezervare))
 
 
+def uimodificare(id , nume , clasa , pret , checkin , lista):
+    try:
+        return modificarezervare(id , nume , clasa , pret , checkin , lista)
+    except ValueError as ve:
+        print("Eroare: {}".format(ve))
+        return lista
+
+
 def command_line_console(lista):
     print (printDescriere())
     lista = []
@@ -40,6 +48,8 @@ def command_line_console(lista):
         listacomenzi = listainput.split(",")
         i = 1
         for i in range(len(listacomenzi)):
+            if listacomenzi[i] == "update":
+                lista = uimodificare(listacomenzi[i+1], listacomenzi[i+2], listacomenzi[i+3], float(listacomenzi[i+4]), listacomenzi[i+5], lista)
             if listacomenzi[i] == "add":
                 lista = uiadaugarezervare(listacomenzi[i+1], listacomenzi[i+2], listacomenzi[i+3], float(listacomenzi[i+4]), listacomenzi[i+5], lista)
                 i = i + 6
